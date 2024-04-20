fbb5af01091562c3360797c73fb6cee8625abe0a
ruzhuo <ruzhuo@517na.com>
2024-04-20 17:00:21 +0800

    ✨ feat(尝试接口调用):

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const corsOptions = {
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
 }
 bootstrap();
